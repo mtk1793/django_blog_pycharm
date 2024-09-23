cd4add60d56d9a6ce942114de8b0c2b547404839
Mahmood Kiasari <mtk1793@gmail.com>
2024-09-22 22:26:59 -0300

    Till 29

--- Readme.txt
+++ Readme.txt
@@ -1,3 +1,5 @@
 2024-09-15 --> Slug is another form of dynamic data that we can pass it to our url path
 
 # Till 26
+
+# Till 29
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
 def index(request):
-    pass
+    return render(request, 'blog/index.html')
+
 
 def posts(request):
-    pass
+    return render(request, 'blog/all-posts.html')
+
 
 def single_post(request):
     pass
